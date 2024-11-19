fb45159331bdf5b369154550ef531b45dc0c63a8
Alex54365 <zjialun2@gmail.com>
2024-11-20 01:12:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ db = SQLAlchemy(app)
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), nullable=False)
     message = db.Column(db.Text, nullable=False)
 
 # 初始化數據庫
