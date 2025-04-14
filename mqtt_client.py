76decd311bbfcb9eb994649039e9b87cc2eb55f8
Alex54365 <zjialun2@gmail.com>
2025-04-14 23:03:51 +0800

    Update mqtt_client.py

--- mqtt_client.py
+++ mqtt_client.py
@@ -23,9 +23,6 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(f" 收到來自 {msg.topic} 的訊息: {msg.payload.decode()}")
 
-# TLS 設定（加入憑證）
-mqtt_client.tls_set(ca_certs=CA_CERT_PATH)
-
 # 如果你用的是自簽憑證，並且主機名不是正式域名，可加這行來避免驗證失敗
 mqtt_client.tls_set(ca_certs="server.crt")
 
